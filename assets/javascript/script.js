e11706bfb9342265cf411e3c3a45c87e0b5388eb
SNAFU <nholdsworth@me.com>
2018-11-15 13:31:30 -0800

    trying to sort out my moment.js logic using manipulations

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -28,15 +28,19 @@ $(document).ready(function () {
         let firstTrainTime = $("#first-train-time").val().trim();
         let frequency = $("#frequency").val().trim();
 
-
         // sends data as a JSON object to firebase 
         database.ref().push({
             trainName: trainName,
             destination: destination,
             firstTrainTime: firstTrainTime,
             frequency: frequency
-            
         });
+
+        // Empty form after table is updated
+        $("#train-name").val("");
+        $("#destination").val("");
+        $("#first-train-time").val("");
+        $("#frequency").val("");
 
     });
 
@@ -73,29 +77,32 @@ $(document).ready(function () {
 
         renderTableRow();
 
-        /*these variables will hold my moment formulas
-            now holds a reference to the current time from moment.js*/
-            let now = moment();
-            console.log(now);
-            
-            // subtracts current time from the last arrival time to give us the number of minutes since the last train
-            let minutesPassed = (now - (childSnapshot.val().firstTrainTime));
-            console.log(minutesPassed)
+        /*these variables will hold my moments
+        now holds a reference to the current time from moment.js*/
+        let now = moment();
+        console.log(now);
 
-            // subtracts the minutes passed since the last train arrived by the Arrival frequency in order to determine how many ninutes away the next train is 
-            // let minutesAway = (parseInt(frequency) - minutesPassed);
-            // console.log(nextArrival);
-            
-            // holds references to the moment calculation updated using the conditional statement below
-            // let nextArrival = (parseInt(now) + parseInt(minutesAway));
-            
-            // If right now equals the time of the first train to arrive plus the amount of time it takes for the next train to arrive then change the firstTrainTime to right now 
-            // if (parseInt(now) === parseInt(childSnapShot.val().firstTrainTime) + parseInt(frequency)) {
-            //     firstTrainTime = now;
-            //     nextArrival = frequency;
-            // };
+        let mostRecentTrain = moment(childSnapshot.val().firstTrainTime);
+        console.log(mostRecentTrain);
 
-            // this variable holds a reference to a new table row HTML element
+        // // subtracts current time from the last arrival time to give us the number of minutes since the last train
+        // console.log(moment(childSnapshot.val().firstTrainTime));
+        // let minutesPassed = parseInt(now) - parseInt(mostRecentTrain);
+
+        // subtracts the minutes passed since the last train arrived by the Arrival frequency in order to determine how many ninutes away the next train is 
+        // let minutesAway = (parseInt(frequency) - minutesPassed);
+        // console.log(nextArrival);
+
+        // holds references to the moment calculation updated using the conditional statement below
+        // let nextArrival = (parseInt(now) + parseInt(minutesAway));
+
+        // If right now equals the time of the first train to arrive plus the amount of time it takes for the next train to arrive then change the firstTrainTime to right now 
+        // if (parseInt(now) === parseInt(childSnapShot.val().firstTrainTime) + parseInt(frequency)) {
+        //     firstTrainTime = now;
+        //     nextArrival = frequency;
+        // };
+
+        // this variable holds a reference to a new table row HTML element
 
     });
 
